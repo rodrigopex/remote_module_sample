74df5344aa394caace0f868bec1be20502503075
Rodrigo Peixoto <rodrigopex@edge.ufal.br>
2023-04-25 16:49:00 -0300

    Adding v2

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,12 @@ pub fn details() -> &'static str {
     if cfg!(feature = "upper") {
         let upper = String::from("I am a remote module.").to_uppercase();
         println!("{upper}");
-        "UPPER"
+        "UPPERv2"
     } else if cfg!(feature = "lower") {
         println!("I am a remote module.");
-        "lower"
+        "lowerv2"
     } else {
-        ""
+        "v2"
     }
 }
 
@@ -18,17 +18,17 @@ mod tests {
     #[cfg(feature = "upper")]
     #[test]
     fn test_details_upper() {
-        assert_eq!(details(), "UPPER");
+        assert_eq!(details(), "UPPERv2");
     }
 
     #[cfg(feature = "lower")]
     #[test]
     fn test_details_lower() {
-        assert_eq!(details(), "lower");
+        assert_eq!(details(), "lowerv2");
     }
     #[cfg(not(any(feature = "upper", feature = "lower")))]
     #[test]
     fn test_details_none() {
-        assert_eq!(details(), "");
+        assert_eq!(details(), "v2");
     }
 }
